7dfd43a1a7d3cc34a5e6a369abc639f32aef6709
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-01-23 00:34:33 -0600

    mik3y implement

--- app/src/main/java/com/lehuman/usbserialmetrics/MainActivity.java
+++ app/src/main/java/com/lehuman/usbserialmetrics/MainActivity.java
@@ -1,27 +1,30 @@
 package com.lehuman.usbserialmetrics;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
 import com.lehuman.usbserialmetrics.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.IOException;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    ActivityMainBinding binding;
+    Metric metric = new Metric();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,40 +33,159 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
+        binding.logView.setMovementMethod(new ScrollingMovementMethod());
+        binding.mik3yBtn.setOnClickListener(view -> {
+            metric.reset();
+            metric.start();
+            mik3yImplement();
+        });
+        binding.felHR85Btn.setOnClickListener(view -> {
+            metric.reset();
+            metric.start();
+            felHR85Implement();
+        });
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                binding.MetricText.setText(metric.toString());
+                binding.MetricText.postDelayed(this, 250);
+            }
+        };
+        binding.MetricText.postDelayed(run, 250);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private static final String ACTION_USB_PERMISSION = "com.lehuman.usbserialmetrics.USB_PERMISSION";
+
+    private void requestUserPermission(UsbManager manager, UsbDevice device) {
+        PendingIntent mPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+        manager.requestPermission(device, mPendingIntent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    void log(String ID, String msg) {
+        if (binding.logView.getLineCount() > 500) {
+            binding.logView.setText("");
+        }
+        binding.logView.append(String.format("[%s] %s\n", ID, msg));
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    boolean visible = false;
+
+    void receive(String ID, byte[] msg) {
+//        binding.logView.append(String.format("(%s) %s\n", ID, new String(msg).replace("\n", "\\n").replace("\t", "\\t")));
+        visible = !visible;
+        binding.Blinker.setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+        metric.newValue(msg);
+    }
+
+    void mik3yImplement() {
+        String ID = "mik3y";
+
+        log(ID, "Starting mik3y implement");
+
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            log(ID, "Driver list empty");
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            log(ID, "Requesting permissions");
+            requestUserPermission(manager, driver.getDevice());
+            return;
+        }
+
+        UsbSerialPort port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        try {
+            port.open(connection);
+            port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        } catch (IOException e) {
+            log(ID, "Failed to open");
+            e.printStackTrace();
+        }
+
+        SerialInputOutputManager usbIoManager = new SerialInputOutputManager(port, new SerialInputOutputManager.Listener() {
+            @Override
+            public void onNewData(byte[] data) {
+                runOnUiThread(() -> receive(ID, data));
+            }
+
+            @Override
+            public void onRunError(Exception e) {
+                log(ID, "Running error");
+            }
+        });
+
+        usbIoManager.start();
+
+        log(ID, "Manager started");
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    void felHR85Implement() {
+
+        String ID = "felHR85";
+
+        log(ID, "Starting felHR85 implement");
+
+//        UsbDevice device = null;
+//        UsbDeviceConnection usbConnection;
+//
+//        String ACTION_NO_USB = "com.felhr.usbservice.NO_USB";
+//
+//        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+//        // This snippet will try to open the first encountered usb device connected, excluding usb root hubs
+//        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+//        if (!usbDevices.isEmpty()) {
+//
+//            // first, dump the hashmap for diagnostic purposes
+//            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+//                device = entry.getValue();
+//            }
+//
+//            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+//                device = entry.getValue();
+//
+//                if (UsbSerialDevice.isSupported(device)) {
+//                    // There is a supported device connected - request permission to access it.
+//        log(ID, "Requesting permissions");
+//                    requestUserPermission(usbManager, device);
+//                    break;
+//                } else {
+//                    usbConnection = null;
+//                    device = null;
+//                }
+//            }
+//            if (device == null) {
+//                // There are no USB devices connected (but usb host were listed). Send an intent to MainActivity.
+//                Intent intent = new Intent(ACTION_NO_USB);
+//                sendBroadcast(intent);
+//            }
+//        } else {
+//            // There is no USB devices connected. Send an intent to MainActivity
+//            Intent intent = new Intent(ACTION_NO_USB);
+//            sendBroadcast(intent);
+//        }
+//
+//        UsbSerialDevice serial = UsbSerialDevice.createUsbSerialDevice(device, usbConnection);
+//
+//        serial.open();
+//        serial.setBaudRate(115200);
+//        serial.setDataBits(UsbSerialInterface.DATA_BITS_8);
+//        serial.setParity(UsbSerialInterface.PARITY_ODD);
+//        serial.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+//
+//        UsbSerialInterface.UsbReadCallback mCallback = data -> {
+//            receive(ID, data);
+//        };
+//
+//        serial.read(mCallback);
     }
+
 }
---
+++ app/src/main/java/com/lehuman/usbserialmetrics/Metric.java
@@ -0,0 +1,104 @@
+package com.lehuman.usbserialmetrics;
+
+
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+
+import java.util.Locale;
+
+public class Metric {
+    public static class MovingAverage {
+
+        private final double[] window;
+        private float sum = 0f;
+        private int fill = 0;
+        private int position;
+
+        public MovingAverage(int size) {
+            this.window = new double[size];
+        }
+
+        public void add(double number) {
+            if (fill == window.length) {
+                sum -= window[position];
+            } else {
+                fill++;
+            }
+            sum += number;
+            window[position++] = number;
+            if (position == window.length) {
+                position = 0;
+            }
+        }
+
+        public double getAverage() {
+            return sum / fill;
+        }
+    }
+
+    boolean run = false;
+
+    long lastUpdate = 0;
+    long lastValue = 0;
+
+    MovingAverage average10 = new MovingAverage(10);
+    MovingAverage average100 = new MovingAverage(100);
+    MovingAverage average1000 = new MovingAverage(1000);
+    MovingAverage averageT10 = new MovingAverage(10);
+    MovingAverage averageT100 = new MovingAverage(100);
+    MovingAverage averageT1000 = new MovingAverage(1000);
+
+    public void start() {
+        if (run)
+            return;
+        run = true;
+        lastUpdate = SystemClock.elapsedRealtime();
+        lastValue = 0;
+    }
+
+    public void stop() {
+        run = false;
+    }
+
+    public void reset() {
+        stop();
+        average10 = new MovingAverage(10);
+        average100 = new MovingAverage(100);
+        average1000 = new MovingAverage(1000);
+        averageT10 = new MovingAverage(10);
+        averageT100 = new MovingAverage(100);
+        averageT1000 = new MovingAverage(1000);
+    }
+
+    public void newValue(byte[] data) {
+        if (!run)
+            return;
+
+        long currentTime = SystemClock.elapsedRealtime();
+        long elapsedTime = currentTime - lastUpdate;
+
+        lastValue = (long) data.length * Byte.SIZE;
+        average10.add(lastValue);
+        average100.add(lastValue);
+        average1000.add(lastValue);
+        averageT10.add(elapsedTime / 1000.0);
+        averageT100.add(elapsedTime / 1000.0);
+        averageT1000.add(elapsedTime / 1000.0);
+        lastUpdate = currentTime;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        try {
+            double avg10 = average10.getAverage() / averageT10.getAverage() / 1000;
+            double avg100 = average100.getAverage() / averageT100.getAverage() / 1000;
+            double avg1000 = average1000.getAverage() / averageT1000.getAverage() / 1000;
+            return String.format(Locale.ENGLISH, "last length : %d\nlast update : %d\naverage10 : %.4f Kbps\naverage100 : %.4f Kbps\naverage1000 : %.4f Kbps", lastValue, lastValue, avg10, avg100, avg1000);
+        } catch (Exception ignored) {
+            return "Error";
+        }
+
+    }
+}
