2944def1a462398c3db6de57c50e1a91c710e90a
LeHuman <47549331+LeHuman@users.noreply.github.com>
2022-01-23 02:57:43 -0600

    icon and clean up

--- app/src/main/java/com/lehuman/usbserialmetrics/MainActivity.java
+++ app/src/main/java/com/lehuman/usbserialmetrics/MainActivity.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        setupUI();
+
         binding.logView.setMovementMethod(new ScrollingMovementMethod());
         binding.mik3yBtn.setOnClickListener(view -> {
             metric.reset();
@@ -58,6 +63,34 @@ public class MainActivity extends AppCompatActivity {
         };
         binding.MetricText.postDelayed(run, 250);
         binding.logView.setText("");
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        hideUI();
+    }
+
+    private void setupUI() {
+        // Account for notches in newer phones
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+        hideUI();
+    }
+
+    private void hideUI() {
+        // Hide Action bar and navigation
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getWindow().getInsetsController().hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+        } else {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 
     private static final String ACTION_USB_PERMISSION = "com.lehuman.usbserialmetrics.USB_PERMISSION";
